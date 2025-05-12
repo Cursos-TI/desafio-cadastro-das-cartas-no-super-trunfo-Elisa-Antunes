4482854a848d9b010b82005447e0f7a41dd3d33a
Elisa-Antunes <elisaantunesdias@gmail.com>
2025-05-12 20:52:48 -0300

    Update países.c
    Analisando

--- países.c
+++ países.c
@@ -1,15 +1,1 @@
-printf("PAIS 01\n);
-  printf("Estado A\n");
-  printf("A01");
-  scanf("%d", &codigo);
-printf("Pouso Alegre")
-  scanf("%[ˆ\n], Nome);
-  printf("152.217");
-  scanf("%d, &população");
-printf("542.797 km²");
-  scanf("%f", &area);
-  printf("R$ 10.700.000.000,00");
-  scanf("%f", &PIB);
-printf("62");
-  scanf("%d", &Pontos turisticos);
-  
+
